f05fc3a45b64b09f2c9af09ba714f6d0fe0d02b3
asoyoshio <asoyoshio@localhost.localdomain>
2018-08-31 10:44:35 +0000

    Update

---
+++ index_files/ajs(1).php
@@ -0,0 +1,9 @@
+(function(){
+var s = "<"+"div id=\'beacon_d426c9d935\' style=\'position: absolute; left: 0px; top: 0px; visibility: hidden;\'>\n";
+s += "<"+"img width=\"0\" height=\"0\" src=\"https://cat.jp.as.criteo.com/delivery/lg.php?cppv=1&cpp=jmFVg3xyQ0dEd2RYbW90RUlNcENzcEgveFVuNU82ZUtnbEszMkxvSEpRb2FQMlRpdFhzMEdWSmh4eU9wQWhsK3A3WWV1SENCM2ZZV29CTFUySnplRm03TkFSRGhuVzZxN1JidWw3Tk9BVjhVOStFdE84dSsxSHRJRzFhQUNCVWpnek8rUXZuSkR5WTBiZkZXQlZldUJ3Q2UwT0lhbjJabDFsMG9VYU1GRmhBc0lJTGFMVEVBZDRtZTRTSXkvbHllVTFGZGhHVmFQTkF4UXEyZThNTVV5N0xYcUFUcHZxcnJzUFVhSTRtZDRBcURQRisrcFFsQ1BoMTlUYUNtSFZkMnluTFVKfA%3D%3D\"/>\n";
+s += "<"+"/div>\n";
+s += "<"+"script type=\'text/javascript\'>\n";
+s += "gCRITEO.passback();\n";
+s += "<"+"/script>\n";
+s += "\n";
+document.write(s);})();
---
+++ index_files/ajs.php
@@ -0,0 +1,9 @@
+(function(){
+var s = "<"+"div id=\'beacon_37800f26f1\' style=\'position: absolute; left: 0px; top: 0px; visibility: hidden;\'>\n";
+s += "<"+"img width=\"0\" height=\"0\" src=\"https://cat.jp.as.criteo.com/delivery/lg.php?cppv=1&cpp=o4J2RXxoQlpYTGY0TnhFTDFuTGI5eHErKzFnTUF5UjhQQU8vOGpibW9pWWJQVjd4UjJvZ3lLRzdwWjhvQXo0ZVJrNEJoblE3WjJiL2JZTnVpZng1THVMblppVW1SY3FPSHRVc0tDT094MHZHTDZVNjZhdVNuOEZLUDIxYnc0czd3MlRETGVkYzB1ckdRRjRNdGQyWlVVMElSZGtBZW9MMmIwRkVvbDcyK1A4ekp4UEpqTXJOZ05PblZpejMyK0ZCalFVZldZWUNRanUvZnJSeVl4bC9Md3RKOE9BbDdKbllBTEZ5b0I1OGVEckYyRHlVdGw4Q1J5alZqekN0RmlqQ2syOXpofA%3D%3D\"/>\n";
+s += "<"+"/div>\n";
+s += "<"+"script type=\'text/javascript\'>\n";
+s += "gCRITEO.passback();\n";
+s += "<"+"/script>\n";
+s += "\n";
+document.write(s);})();
